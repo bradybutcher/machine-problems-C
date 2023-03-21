76f9136972f9a179ba3137469a37238531b03fc7
bradybutcher <bbutcher@wccnet.edu>
2023-03-21 10:42:37 -0400

    add description

--- machineprob06/bbutcher.cpp
+++ machineprob06/bbutcher.cpp
@@ -1,10 +1,20 @@
+/*
+ * Brady Butcher
+ * 03.14.2023
+ * CPS 171 - 01
+ *
+ * This program will read a table of characters from an input file, fill it into a 2-dimensional 8 by 8 array, and finds the words in the table using a word bank provided in the input file. The program will then output the words found in the table to an output file.
+ */
+
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <array>
 
 using namespace std;
 
 const int arrSize = 8;
+char array[arrSize][arrSize];
 
 string ifile_path("input.txt");
 string ofile_path("output.txt");
